e79b625a2b2194dfd937665d097c2470509f910a
Ilya Starchenko <redux1234567@mail.ru>
2022-02-24 22:14:52 +0600

    simplify test case

--- ms-config-constructor/src/main/kotlin/configconstructor/controllers/rest/ConfigRestController.kt
+++ ms-config-constructor/src/main/kotlin/configconstructor/controllers/rest/ConfigRestController.kt
@@ -1,54 +1,22 @@
 package configconstructor.controllers.rest
 
-import configconstructor.services.facades.ConfigService
 import dataflow.commons.security.AuthorizationGrpcClient
-import dataflow.libconfigconstructor.dto.ConfigCreateDto
-import dataflow.libconfigconstructor.dto.ConfigDto
-import dataflow.libconfigconstructor.dto.ConfigUpdateDto
-import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/configs"])
-class ConfigRestController(val configService: ConfigService, val auth: AuthorizationGrpcClient) {
+class ConfigRestController(val auth: AuthorizationGrpcClient) {
 
     //
     // Read
     //
 
     @GetMapping
-    fun getConfigsPage(): String {
+    fun test(): String {
         val res = auth.getRuleByName("test")
         return res.condition
-    }
-
-    //
-    // Create
-    //
-
-
-    @PostMapping
-    fun createConfig(@RequestBody configCreateDto: ConfigCreateDto): ConfigDto {
-        return configService.create(configCreateDto, null)
-    }
-
-    //
-    // Update
-    //
-
-    @PutMapping
-    fun updateConfig(@RequestBody configUpdateDto: ConfigUpdateDto): ConfigDto {
-        return configService.updateById(configUpdateDto, null)
-    }
-
-    //
-    // Delete
-    //
-
-    @DeleteMapping("/{configId}")
-    fun updateConfig(@PathVariable configId: UUID) {
-        configService.deleteById(configId)
     }
 
 }
